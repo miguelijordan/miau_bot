43593d912e934909af958d4a2cbe1e17af632cb2
José Miguel Horcas Aguilera <horcas@lcc.uma.es>
2016-06-30 13:16:15 +0200

    refactor: change scope of the variables

--- miau/utils/Persistence.py
+++ miau/utils/Persistence.py
@@ -1,30 +1,30 @@
 from pymongo import MongoClient
 from miau.constants import constants
 
-class Persistence():
+class Persistence:
     def __init__(self, collection_name):
-        self.client = MongoClient()
-        self.db = self.client[constants.MIAU_DB]
-        self.collection = self.db[collection_name]
-        self.data = self.getData()              # list in memory for efficiency
+        self.__client = MongoClient()
+        self.__db = self.__client[constants.MIAU_DB]
+        self.__collection = self.__db[collection_name]
+        self.__data = self.getData()              # list in memory for efficiency
 
     def getData(self):
-        return list(self.collection.find())
-        
+        return list(self.__collection.find())
+
     def save(self, element):
-        self.collection.insert_one(element)
-        self.data.append(element)
+        self.__collection.insert_one(element)
+        self.__data.append(element)
 
     def delete(self, element):
-        self.collection.delete_one(element)
+        self.__collection.delete_one(element)
         if element in self.data:
-            self.data.remove(element)
+            self.__data.remove(element)
 
     def deleteAll(self, element):
-        self.collection.delete_many(element)
+        self.__collection.delete_many(element)
         while element in self.data:
-            self.data.remove(element)
+            self.__data.remove(element)
 
     def clearData(self):
-        self.collection.drop()
-        self.data = []
+        self.__collection.drop()
+        self.__data = []
