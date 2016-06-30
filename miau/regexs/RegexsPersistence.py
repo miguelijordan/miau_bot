cd314d02eebf0878353cc35e6deee288807fd77f
José Miguel Horcas Aguilera <horcas@lcc.uma.es>
2016-06-30 17:14:31 +0200

    refactoring the class

--- miau/regexs/RegexsPersistence.py
+++ miau/regexs/RegexsPersistence.py
@@ -1,48 +1,42 @@
-from pymongo import MongoClient     # Python driver for MongoDB
 import re                           # Regular expression operations
-from miau.constants import constants
 
-class RegexsPersistence():
-    def __init__(self):
-        self.client = MongoClient()
-        self.db = self.client[constants.MIAU_DB]
-        self.collection = self.db[constants.REGEXS]
-        self.regexs = self.__getCompiledRegexs()
 
-    def addRegex(self, regex):
-        prog = None
+class RegexsPersistence(Persistence):
+    """ Elements are dicts with the following keys: 'pattern', 'answer'
+    Regexs are dicts with the following keys: 'pattern', 'answer', 'compiledRegex'.
+    """
+
+    def __getRegex(self, element):
+        regex = dict(element)
+        regex['compiledRegex'] = re.compile(element['pattern'], re.IGNORECASE)
+        return regex
+
+    def save(self, element):
+        """ Return True if the element was saved correctly, False in othercase
+        in which the regex's pattern is not a valid regular expression.
+        """
         try:
-            prog = re.compile(regex['pattern'])
-        except:
-            raise    # To do: raise and send bot message.
+            regex = self.__getRegex(element)
+            super().save(regex)
+            return True
+        except re.error:
+            return False
 
-        if prog:    
-            newRegex = dict(regex)
-            newRegex['compiledRegex'] = prog
-            self.regexs.append(newRegex)
+    def delete(self, element):
+        regex = self.__getRegex(element)
+        super().delete(regex)
 
-            self.collection.insert_one(regex)   # Acceso a BD
-
-
-    def deleteRegex(self, regex):
-        self.collection.remove(regex)       # Acceso a BD
-
-        elements = list(filter(lambda x : x['pattern'] == regex['pattern'] and x['answer'] == regex['answer'], self.regexs))
-        for e in elements:
-            self.regexs.remove(e)
-
-    def getRegexs(self):
+    def getData(self):
         return list(self.collection.find().sort('pattern'))   # Acceso a BD
 
     def getMatchingRegexs(self, text):
-        matchings = list(filter(lambda x : re.search(x['compiledRegex'], text), self.regexs))
-        return matchings
+        return matchings list(filter(lambda x: re.search(x['compiledRegex'], text), self.getData))
 
     def getBestMatchingRegexs(self, text):
         matchings = self.getMatchingRegexs(text)
-        maxMatching = max(matchings, key=lambda x : len(re.search(x['compiledRegex'], text).group(0)))
+        maxMatching = max(matchings, key=lambda x: len(re.search(x['compiledRegex'], text).group(0)))
         maxLength = len(re.search(maxMatching['compiledRegex'], text).group(0))
-        bestMatchings = list(filter(lambda x : len(re.search(x['compiledRegex'], text).group(0)) == maxLength, matchings))
+        bestMatchings = list(filter(lambda x: len(re.search(x['compiledRegex'], text).group(0)) == maxLength, matchings))
         return bestMatchings
 
     def __getCompiledRegexs(self):
@@ -50,7 +44,3 @@ class RegexsPersistence():
         for r in regexs:
             r['compiledRegex'] = re.compile(r['pattern'], re.IGNORECASE)
         return regexs
-
-    def clearRegexs(self):
-        self.collection.drop()
-        self.regexs = []
